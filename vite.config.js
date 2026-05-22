e851c29923e9fdd9176d7191bf11050c3a804d9f
Sreya Kambhatla <145293962+sreya-kambhatla@users.noreply.github.com>
2026-05-22 08:13:49 -0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 export default defineConfig({
-  base: '/OTI-Tracker/',
+  base: '/OTI-Tracker-AI/',
   plugins: [react()],
 });
