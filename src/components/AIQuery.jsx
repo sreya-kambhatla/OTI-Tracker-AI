5139be100270b8aaca14f83ca7662bbcbdd103f1
Sreya Kambhatla <145293962+sreya-kambhatla@users.noreply.github.com>
2026-05-23 11:13:52 -0700

    Update AIQuery.jsx

--- src/components/AIQuery.jsx
+++ src/components/AIQuery.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 
-
 function AIQuery({ onApplyFilters, allLogs }) {
   const [query,   setQuery]   = React.useState("");
   const [loading, setLoading] = React.useState(false);
